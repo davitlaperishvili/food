bcedc9b5dc1649a42c318b74f0b68503e61944ad
Davit Laperishvili <>
2020-09-29 18:41:02 +0400

    lesson: 21

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,52 @@
 "use strict";
 
-// const numberOfFilms = prompt('how many films you have watched', '');
+// // const numberOfFilms = prompt('how many films you have watched', '');
 
-// const personalMoviesDB = {
-//     count: numberOfFilms,
-//     movies: {},
-//     actors: {},
-//     genres: [],
-//     privat: false
-// };
+// // const personalMoviesDB = {
+// //     count: numberOfFilms,
+// //     movies: {},
+// //     actors: {},
+// //     genres: [],
+// //     privat: false
+// // };
 
 
 
-// for( let i = 0; i < 2; i++){
-//     const a = prompt('The last movie you watched', ''),
-//           b = prompt('How much do you like it?', '');
+// // for( let i = 0; i < 2; i++){
+// //     const a = prompt('The last movie you watched', ''),
+// //           b = prompt('How much do you like it?', '');
 
-//     if( a != null && b != null && a != '' && b != '' && a.length < 50 ){
-//         console.log('done')
-//         personalMoviesDB.movies[a] = b;
-//     }else{
-//         i--
-//         console.log('error')
-//     }
-//     if(personalMoviesDB.count < 10){
-//         console.log('<10')
-//     }
+// //     if( a != null && b != null && a != '' && b != '' && a.length < 50 ){
+// //         console.log('done')
+// //         personalMoviesDB.movies[a] = b;
+// //     }else{
+// //         i--
+// //         console.log('error')
+// //     }
+// //     if(personalMoviesDB.count < 10){
+// //         console.log('<10')
+// //     }
+// // }
+
+// // console.log(personalMoviesDB);
+
+// function ret (){
+//     let num = 50;
+//     return num
 // }
+// console.log(ret())
 
-// console.log(personalMoviesDB);
 
-function ret (){
-    let num = 50;
-    return num
+const options = {
+    name: 'object',
+    width: 1024,
+    height: 1024,
+    color: {
+        border: 'black',
+        bg: 'red'
+    }
+};
+//console.log(`${options[name]}`);
+for( let key in options){
+    console.log(`Option ${key} have value: ${options[key]} `);
 }
-console.log(ret())
-
-
